3c4e4b2950b48c8741ca1699369fad76900bcb0d
Patrick McCoy <patrick@pjmccoy.com>
2012-04-20 13:49:58 -0600

    Added more methods and added Unit tests

--- Utility.class.php
+++ Utility.class.php
@@ -1,6 +1,277 @@
 <?php
 
 class Utility {
+    
+    /**
+     * List of all known HTTP response codes - used to
+     * translate numeric codes to messages.
+     *
+     * @var array
+     */
+    protected static $messages = array(
+        // Informational 1xx
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+
+        // Success 2xx
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+
+        // Redirection 3xx
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',  // 1.1
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        // 306 is deprecated but reserved
+        307 => 'Temporary Redirect',
+
+        // Client Error 4xx
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+
+        // Server Error 5xx
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        509 => 'Bandwidth Limit Exceeded'
+    );
+
+    /**
+     * List of all acceptable HTTP response codes - used to
+     * check the passed error code is handled by the class
+     *
+     * @var array
+     */
+    public static $codes = array(
+        // 100
+          100
+        , 101
+
+        // 200
+        , 200
+        , 201
+        , 202
+        , 203
+        , 204
+        , 205
+        , 206
+
+        // 300
+        , 300
+        , 301
+        , 302
+        , 303
+        , 304
+        , 305
+        , 307
+
+        // 400
+        , 400
+        , 401
+        , 402
+        , 403
+        , 404
+        , 405
+        , 406
+        , 410
+
+        // 500
+        , 500
+        , 501
+        , 502
+        , 503
+        , 509
+    );
+
+    /**
+     * Validate an Email Address
+     *
+     * @param $email
+     * @return mixed - $email if valid, FALSE if not
+     */
+    public static function validateEmail($email) {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * Validate an IP Address and make sure that it is not in any private or reserved ranges
+     *
+     * @param $ip
+     * @return mixed - $ip if valid, FALSE if not
+     */
+    public static function validateIP($ip) {
+        return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE);
+    }
+
+    /**
+     * Validate an CIDR Range and make sure that it is not in any private or reserved ranges
+     *
+     * @param $cidr
+     * @return boolean
+     */
+    public static function validateCIDR($cidr) {
+        // check that we are actually dealing with a CIDR here
+        if (!self::isCIDR($cidr)) {
+            return false;
+        }
+
+        // Get the base and the bits from the CIDR
+        list($base, $bits) = explode('/', $cidr);
+
+        list($low, $high) = self::getCidrRange($cidr, true);
+
+        // CIDR base must start at the low end of the range
+        if (ip2long($base) != $low) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Break a CIDR into its high and low ranges
+     *
+     * @param $cidr string the cidr to get the low and high ranges for
+     * @param $int bool return the results as integers (true) or strings (false)
+     * @return array an array of ($low, $high)
+     */
+    public static function getCidrRange($cidr, $int = true) {
+         // Get the base and the bits from the CIDR
+        list($base, $bits) = explode('/', $cidr);
+
+        // Now split it up into it's classes
+        list($a, $b, $c, $d) = explode('.', $base);
+
+        // Now do some bit shifting/switching to convert to ints
+        $i    = ($a << 24) + ($b << 16) + ($c << 8) + $d;
+        $mask = ($bits == 0) ? 0 : (~0 << (32 - $bits));
+
+        // Here's our lowest int
+        $low = $i & $mask;
+        // Here's our highest int
+        $high = $i | (~$mask & 0xFFFFFFFF);
+
+        if ($int) {
+            return array($low, $high);
+        } else {
+            return array(long2ip($low), long2ip($high));
+        }
+
+    }
+
+    /**
+     * Redirect the user's browser to another page
+     *
+     * @param $uri
+     */
+    public static function redirect($uri) {
+        header('Location: ' . $uri);
+        exit();
+    }
+    /**
+     * utility version of this function from the util function include
+     *
+     * escapes html to make it safe for displaying.
+     *
+     * @param string $string
+     * @param int $quote_style
+     * @param string $charset
+     * @return type
+     */
+    public static function escapeHtml($string, $quote_style = ENT_COMPAT, $charset = 'UTF-8') {
+
+        $decoded_string = htmlentities($string, $quote_style, $charset, false);
+        if (!$decoded_string) {
+            //wasn't able to do decoding, probably due to an invalid multi-byte character
+            //detect character encoding
+            $encoding_list = array('UTF-8', 'ISO-8859-1');
+            $encoding = mb_detect_encoding($string, $encoding_list);
+
+            // try converting again using that encoding
+            if (in_array($encoding, $encoding_list) && mb_check_encoding($string, $encoding)) {
+                $decoded_string = htmlentities($string, $quote_style, $encoding, false);
+            }
+
+            if (!$decoded_string) {
+                return $string;
+            }
+        }
+        return $decoded_string;
+    }
+
+    // return boolean
+    // check to see if string is a CIDR range
+    public static function isCIDR($cidr) {
+
+        $cidr_regex = '/^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]))\/([1-9]|[1-2]\d|3[0-2])$/';
+        $matches = array();
+        if (preg_match($cidr_regex, $cidr, $matches)) {
+            if ($matches[1] && $matches[6]) {
+               return (bool) self::validateIP($matches[1]);
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Send an HTTP Status Code Header
+     *
+     * @param $code - the http code you want to send
+     * @return bool - if the status code was sent
+     */
+    public static function HTTPStatus($code = 200) {
+        if (in_array($code, self::$codes)) {
+            header($_SERVER['SERVER_PROTOCOL'] .' '. $code .' '. self::$messages[$code], true, $code);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Generate a random string of characters
+     *
+     * @param $length - the number of random characters to return
+     * @return string - the random string of characters
+     */
+    public static function RandomString($length = 16) {
+        $string = '';
+        //create confirmation id, 16 chars long
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        $len = strlen($chars) - 1;
+        $salt = '';
+        for ($p = 0; $p < $length; $p++) {
+            $string .= $chars[mt_rand(0, $len)];
+        }
+        return $string;
+    }
 
     /**
      * humanFilesize method
@@ -15,5 +286,5 @@ class Utility {
         $sz = 'BKMGTP';  
         $factor = floor((strlen($bytes) - 1) / 3);  
         return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];  
-    }  
+    }
 }
---
+++ test/UtilityTest.php
@@ -0,0 +1,343 @@
+<?php
+require_once __DIR__ . '/../Utility.class.php';
+
+/**
+ * Test class for Common_Utility.
+ */
+class utilityTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp() {
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown() {
+    }
+
+    /**
+     * Test the IP validation.
+     */
+    public function test_validate_ip() {
+        $fixtures = array(
+            // Valid IP
+              array('ip'=>'12.13.14.15', 'result'=>'12.13.14.15')
+
+            // Invalid IP
+            , array('ip'=>'256.256.256.256', 'result'=>FALSE)
+
+            // Invalid IP
+            , array('ip'=>'256.0.0.0', 'result'=>FALSE)
+
+            // Invalid IP
+            , array('ip'=>'255.256.0.0', 'result'=>FALSE)
+
+            // Invalid IP
+            , array('ip'=>'255.255.256.0', 'result'=>FALSE)
+
+            // Invalid IP
+            , array('ip'=>'255.24.245.256', 'result'=>FALSE)
+
+            // Invalid IP
+            , array('ip'=>'2563.0325.1340.01324', 'result'=>FALSE)
+
+            // Invalid IP
+            , array('ip'=>'25a.0.0.0', 'result'=>FALSE)
+
+            // Private Range
+            , array('ip'=>'10.0.0.0', 'result'=>FALSE)
+
+            // Private Range
+            , array('ip'=>'10.255.255.255', 'result'=>FALSE)
+
+            // Private Range
+            , array('ip'=>'192.168.0.0', 'result'=>FALSE)
+
+            // Private Range
+            , array('ip'=>'192.168.255.255', 'result'=>FALSE)
+
+            // Private Range
+            , array('ip'=>'172.16.0.0', 'result'=>FALSE)
+
+            // Private Range
+            , array('ip'=>'172.31.255.255', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'0.0.0.0', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'0.255.255.255', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'169.254.0.0', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'169.254.255.255', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'192.0.2.0', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'192.0.2.255', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'224.0.0.0', 'result'=>FALSE)
+
+            // Reserved Range
+            , array('ip'=>'224.255.255.255', 'result'=>FALSE)
+
+            // Invalid IP
+            , array('ip'=>'255.255.255.0', 'result'=>FALSE)
+        );
+
+        foreach ($fixtures as $fixture) {
+            $this->assertEquals($fixture['result'], Utility::validateIP($fixture['ip']));
+        }
+    }
+
+    /**
+     * Test the IP validation.
+     */
+    public function test_validate_email() {
+        $fixtures = array(
+            // Valid Email
+              array('email'=>'niceandsimple@example.com', 'result'=>'niceandsimple@example.com')
+
+            // Valid Email
+            , array('email'=>'a.little.unusual@example.com', 'result'=>'a.little.unusual@example.com')
+
+            // Valid Email
+            , array('email'=>'a.little.more.unusual@dept.example.com', 'result'=>'a.little.more.unusual@dept.example.com')
+
+            // Valid Email
+            , array('email'=>'much."more\ unusual"@example.com', 'result'=>'much."more\ unusual"@example.com')
+
+            // Valid Email
+            , array('email'=>'very.unusual."@".unusual.com@example.com', 'result'=>'very.unusual."@".unusual.com@example.com')
+
+            // Valid Email
+            , array('email'=>'very."(),:;<>[]".VERY."very@\\ very".unusual@strange.example.com', 'result'=>'very."(),:;<>[]".VERY."very@\\ very".unusual@strange.example.com')
+
+            // Invalid Email
+            , array('email'=>'Abc.example.com', 'result'=>FALSE)
+
+            // Invalid Email
+            , array('email'=>'Abc.@example.com', 'result'=>FALSE)
+
+            // Invalid Email
+            , array('email'=>'Abc..123@example.com', 'result'=>FALSE)
+
+            // Invalid Email
+            , array('email'=>'A@b@c@example.com', 'result'=>FALSE)
+
+            // Invalid Email
+            , array('email'=>'a"b(c)d,e:f;g<h>i[j\k]l@example.com', 'result'=>FALSE)
+
+            // Invalid Email
+            , array('email'=>'just"not"right@example.com', 'result'=>FALSE)
+
+            // Invalid Email
+            , array('email'=>'this is"not\allowed@example.com', 'result'=>FALSE)
+
+            // Invalid Email
+            , array('email'=>'this\ still\"not\\allowed@example.com', 'result'=>FALSE)
+        );
+
+        foreach ($fixtures as $fixture) {
+            $this->assertEquals($fixture['result'], Utility::validateEmail($fixture['email']));
+        }
+    }
+
+    /**
+     * Test the validateCIDR method
+     */
+    public function test_validateCIDR() {
+        $fixtures = array(
+            array('CIDR' => '', 'result' => FALSE)
+            , array('CIDR' => '8.0.0.0/8', 'result' => TRUE)
+            , array('CIDR' => '8.8.0.0/8', 'result' => FALSE)
+            , array('CIDR' => '8.8.0.0/16', 'result' => TRUE)
+            , array('CIDR' => '8.8.2.0/16', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.0/24', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.0/25', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.128/25', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.255/25', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.0/26', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.64/26', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.128/26', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.255/26', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.0/27', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.32/27', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.64/27', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.33/27', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.0/28', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.16/28', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.32/28', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.222/28', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.208/28', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.0/29', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.8/29', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.16/29', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.17/29', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.15/29', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.0/30', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.4/30', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.5/30', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.7/30', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.8/30', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.0/31', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.2/31', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.1/31', 'result' => FALSE)
+            , array('CIDR' => '8.8.8.0/32', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.1/32', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.34/32', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.234/32', 'result' => TRUE)
+            , array('CIDR' => '8.8.8.255/32', 'result' => TRUE)
+            , array('CIDR' => '209.44.107.222/28', 'result' => FALSE)
+            , array('CIDR' => '209.44.107.208/28', 'result' => TRUE)
+            , array('CIDR' => '209.44.107.224/28', 'result' => TRUE)
+        );
+
+        foreach ($fixtures as $fixture) {
+            $this->assertEquals($fixture['result'], Utility::validateCIDR($fixture['CIDR']));
+        }
+    }
+
+    /**
+     * Test the getCidrRange method
+     */
+    public function test_getCidrRange() {
+        $fixtures = array(
+              array('CIDR' => '8.8.8.8/32', 'result_low' =>'8.8.8.8', 'result_high' =>'8.8.8.8')
+            , array('CIDR' => '8.8.8.0/31', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.1')
+            , array('CIDR' => '8.8.8.0/30', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.3')
+            , array('CIDR' => '8.8.8.0/29', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.7')
+            , array('CIDR' => '8.8.8.0/28', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.15')
+            , array('CIDR' => '8.8.8.0/27', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.31')
+            , array('CIDR' => '8.8.8.0/26', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.63')
+            , array('CIDR' => '8.8.8.0/25', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.127')
+            , array('CIDR' => '8.8.8.0/24', 'result_low' =>'8.8.8.0', 'result_high' =>'8.8.8.255')
+            , array('CIDR' => '8.8.0.0/16', 'result_low' =>'8.8.0.0', 'result_high' =>'8.8.255.255')
+            , array('CIDR' => '8.0.0.0/8', 'result_low' =>'8.0.0.0', 'result_high' =>'8.255.255.255')
+            , array('CIDR' => '209.44.107.222/28', 'result_low' =>'209.44.107.208', 'result_high' =>'209.44.107.223')
+        );
+
+        // test the int results
+        foreach ($fixtures as $fixture) {
+            list($low, $high) = Utility::getCidrRange($fixture['CIDR'], true);
+            $this->assertEquals(ip2long($fixture['result_low']), $low);
+            $this->assertEquals(ip2long($fixture['result_high']), $high);
+        }
+
+        // test the string results
+        foreach ($fixtures as $fixture) {
+            list($low, $high) = Utility::getCidrRange($fixture['CIDR'], false);
+            $this->assertEquals($fixture['result_low'], $low);
+            $this->assertEquals($fixture['result_high'], $high);
+        }
+    }
+
+    /**
+     * Test the isCIDR method
+     */
+    public function test_isCIDR() {
+        $fixtures = array(
+              array('CIDR' => '', 'result' => FALSE)
+            , array('CIDR' => '1.2.3.4/32', 'result' => TRUE)
+            , array('CIDR' => '1.2.3.4/24', 'result' => TRUE)
+            , array('CIDR' => '1.2.3.4/16', 'result' => TRUE)
+            , array('CIDR' => '1.2.3.4/8', 'result' => TRUE)
+            , array('CIDR' => '1.2.3.4/4', 'result' => TRUE)
+            , array('CIDR' => '1.2.3./32', 'result' => FALSE)
+            , array('CIDR' => '1.2.3/32', 'result' => FALSE)
+            , array('CIDR' => '1.2.3/24', 'result' => FALSE)
+            , array('CIDR' => '1.2.3/16', 'result' => FALSE)
+            , array('CIDR' => '1.2.3/8', 'result' => FALSE)
+            , array('CIDR' => '1.2.3/4', 'result' => FALSE)
+            , array('CIDR' => '1.2.3.4/33', 'result' => FALSE)
+            , array('CIDR' => '1.2.3.4/45', 'result' => FALSE)
+            , array('CIDR' => '127.0.0.1/32', 'result' => FALSE)
+
+        );
+
+        foreach ($fixtures as $fixture) {
+            $this->assertEquals($fixture['result'], Utility::isCIDR($fixture['CIDR']));
+        }
+    }
+
+    /**
+     * Test the random string generation
+     */
+    public function test_RandomString() {
+        // test various lengths and assert that the function returns the correct length
+        $this->assertEquals(16, strlen(Utility::RandomString()));
+        $this->assertEquals(10, strlen(Utility::RandomString(10)));
+        $this->assertEquals(32, strlen(Utility::RandomString(32)));
+        $this->assertEquals(64, strlen(Utility::RandomString(64)));
+        $this->assertEquals(128, strlen(Utility::RandomString(128)));
+        $this->assertEquals(256, strlen(Utility::RandomString(256)));
+        $this->assertEquals(512, strlen(Utility::RandomString(512)));
+        $this->assertEquals(1024, strlen(Utility::RandomString(1024)));
+
+        // now test the actual return values are what we expect
+        mt_srand(50);
+        $this->assertEquals('fIO5PlY9m0', Utility::RandomString(10));
+
+        mt_srand(100);
+        $this->assertEquals('cpRZjd009zHLUxLc', Utility::RandomString());
+
+        mt_srand(125);
+        $this->assertEquals('e86xWKICI8NBeXZ9j5fHUxIu3h4CJqgK', Utility::RandomString(32));
+
+        mt_srand(150);
+        $this->assertEquals('FDtZ2iQoSidoDp4XE2w5HTn44DvBXhOtAaYpsxcodpc3iukf3T1zqOedxWXZVdeK', Utility::RandomString(64));
+
+        mt_srand(200);
+        $this->assertEquals('CfwJkujjOyA3Wb4tcoB71mAUESSlJH2LwNur3d6nL8gL2Pj6bjTga7nyYWGUCHvmqKUwBSM48jR5o4QfyxZepodt0dKrBkSgOtxmGp938rMSWafvx9TX8QWOdSXGSrHJ', Utility::RandomString(128));
+
+    }
+    
+    /**
+     * Test the Human Filesize method
+     */
+    public function test_humanFilesize() {
+        $fixtures = array(
+              array('bytes' => 1, 'decimals' => 0, 'result' => '1B')
+            , array('bytes' => 10, 'decimals' => 0, 'result' => '10B')
+            , array('bytes' => 100, 'decimals' => 0, 'result' => '100B')
+            , array('bytes' => 1024, 'decimals' => 0, 'result' => '1K')
+            , array('bytes' => 1024*2, 'decimals' => 0, 'result' => '2K')
+            , array('bytes' => pow(1024,2), 'decimals' => 0, 'result' => '1M')
+            , array('bytes' => pow(1024,3), 'decimals' => 0, 'result' => '1G')
+            , array('bytes' => pow(1024,4), 'decimals' => 0, 'result' => '1T')
+            , array('bytes' => pow(1024,5), 'decimals' => 0, 'result' => '1P')
+            , array('bytes' => 1, 'decimals' => 1, 'result' => '1.0B')
+            , array('bytes' => 10, 'decimals' => 1, 'result' => '10.0B')
+            , array('bytes' => 100, 'decimals' => 1, 'result' => '100.0B')
+            , array('bytes' => 1024, 'decimals' => 1, 'result' => '1.0K')
+            , array('bytes' => 1024*2, 'decimals' => 1, 'result' => '2.0K')
+            , array('bytes' => pow(1024,2), 'decimals' => 1, 'result' => '1.0M')
+            , array('bytes' => pow(1024,3), 'decimals' => 1, 'result' => '1.0G')
+            , array('bytes' => pow(1024,4), 'decimals' => 1, 'result' => '1.0T')
+            , array('bytes' => pow(1024,5), 'decimals' => 1, 'result' => '1.0P')
+            , array('bytes' => 1, 'decimals' => 2, 'result' => '1.00B')
+            , array('bytes' => 10, 'decimals' => 2, 'result' => '10.00B')
+            , array('bytes' => 100, 'decimals' => 2, 'result' => '100.00B')
+            , array('bytes' => 1024, 'decimals' => 2, 'result' => '1.00K')
+            , array('bytes' => 1024*2, 'decimals' => 2, 'result' => '2.00K')
+            , array('bytes' => pow(1024,2), 'decimals' => 2, 'result' => '1.00M')
+            , array('bytes' => pow(1024,3), 'decimals' => 2, 'result' => '1.00G')
+            , array('bytes' => pow(1024,4), 'decimals' => 2, 'result' => '1.00T')
+            , array('bytes' => pow(1024,5), 'decimals' => 2, 'result' => '1.00P')
+        );
+        
+        foreach ($fixtures as $fixture) {
+            $this->assertEquals($fixture['result'], Utility::humanFilesize($fixture['bytes'], $fixture['decimals']));
+        }
+    }
+}
